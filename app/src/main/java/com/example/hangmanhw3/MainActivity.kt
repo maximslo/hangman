07d755fc36bfb6a741d080145bdb839e854a08c9
Maxim Slobodchikov <maxims@bu.edu>
2024-02-15 18:15:17 -0500

    create rest of logic functions, notInWord, randomSet, vowelHint, hintClick, updateLetter

--- app/src/main/java/com/example/hangmanhw3/MainActivity.kt
+++ app/src/main/java/com/example/hangmanhw3/MainActivity.kt
@@ -161,5 +161,116 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    private fun underscoreWord() {
+        val stringBuilder = StringBuilder()
+        for (char in currWord) {
+            stringBuilder.append("_ ")
+        }
+        guessingWord.text = stringBuilder.toString()
+    }
+
+    private fun resetButtons() {
+        allLetters.forEach { button ->
+            button.isEnabled = true
+            button.setBackgroundColor(Color.parseColor("#42474f"))
+        }
+        guessedLetters.clear()
+    }
+
+    fun letterClick(view: View) {
+        if (view is Button) {
+            val button = view as Button
+            guessedLetters.add(button)
+            button.setBackgroundColor(Color.parseColor("#c6cfc8"))
+            button.isEnabled = false
+            val guess = view.text.toString().uppercase().first()
+            if (currWord.contains(guess, ignoreCase = true)) {
+                currWord.forEachIndexed { index, char ->
+                    if (char.equals(guess, ignoreCase = true)) {
+                        updateLetter(index, guess)
+                    }
+                }
+            } else {
+                wrongLetter()
+            }
+            checkWin()
+        }
+    }
+
+    private fun updateLetter(index: Int, newChar: Char) {
+        val currentText = StringBuilder(guessingWord.text.toString())
+        currentText.setCharAt(index*2, newChar)
+        guessingWord.text = currentText.toString()
+    }
+
+    private fun checkWin(){
+        val trimmedWord = guessingWord.text.toString().replace(" ", "")
+        if(trimmedWord.equals(currWord, ignoreCase = true)){
+            disableAll()
+            val snackbar = Snackbar.make(findViewById(android.R.id.content), "You Won! Click to start a new game :)", Snackbar.LENGTH_INDEFINITE)
+            snackbar.setAction("New Game") {
+                newGame()
+                snackbar.dismiss()
+            }
+            snackbar.show()
+        }
+    }
+
+    fun hintClick(view: View) {
+        if (numGuesses == 6 && hintState >= 1) {
+            val snackbar = Snackbar.make(view,
+                "Hint not available",
+                Snackbar.LENGTH_LONG)
+            snackbar.show()
+        }
+        else{
+            if(hintState == 0){
+                hintText.append(currHint)
+            }
+            else if(hintState == 1)
+                disableHalf()
+            else if(hintState == 2){
+                vowelHint()
+            }
+            hintState += 1
+        }
+    }
+
+    private fun vowelHint() {
+        wrongLetter() //costs the player a turn
+        val vowels = setOf('A', 'E', 'I', 'O', 'U')
+        for ((index, char) in currWord.withIndex()) {
+            if (char.uppercaseChar() in vowels && guessingWord.text.toString()[index*2] == '_') {
+                updateLetter(index, char.uppercaseChar())
+            }
+        }
+        checkWin()
+        hintText.text = "vowel hint!"
+    }
+
+    private fun disableHalf() {
+        wrongLetter()
+        val lettersNotInWord = notInWord()
+        val lettersToDisable = randomSet(lettersNotInWord)
+
+        allLetters.forEach { button ->
+            val char = button.text.toString().uppercase().first()
+            if (char in lettersToDisable) {
+                button.isEnabled = false
+                button.setBackgroundColor(Color.parseColor("#c6cfc8"))
+            }
+        }
+    }
+
+    private fun notInWord(): Set<Char> {
+        val wordLetters = currWord.uppercase().toSet()
+        return ('A'..'Z').toSet() - wordLetters
+    }
+
+    private fun randomSet(letters: Set<Char>): Set<Char> {
+        val list = letters.toList()
+        val halfSize = list.size / 2
+        return list.shuffled().take(halfSize).toSet()
+    }
 
 }
