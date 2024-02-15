6aeeda562955bcdc2025c13f1ff535276b3318bb
Maxim Slobodchikov <maxims@bu.edu>
2024-02-15 17:40:20 -0500

    added new imports, variables, layout

--- app/src/main/java/com/example/hangmanhw3/MainActivity.kt
+++ app/src/main/java/com/example/hangmanhw3/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.hangmanhw3
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var hangmanProgress: ImageView
+    private lateinit var guessingWord: TextView
+    private lateinit var newGameButton: Button
+    private lateinit var currList: List<String>
+    private lateinit var hintText: TextView
+    private lateinit var hintButton: Button
+
+    private var currPair: String = ""
+    private var currWord: String = ""
+    private var currHint: String = ""
+    private var numGuesses = 0
+    private var hintState = 0
+    private val guessedLetters = mutableSetOf<Button>()
+    private val allLetters = mutableSetOf<Button>()
+    companion object {
+        const val GUESSING_WORD_KEY = "GUESSING_WORD_KEY"
     }
+
 }
